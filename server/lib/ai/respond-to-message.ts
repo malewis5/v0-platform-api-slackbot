68ea34f0040f81c284817b1dbe2c9303f59643d5
malewis5 <matthew.a.lewis7@gmail.com>
2025-08-02 09:38:12 -0400

    fix prompt

--- server/lib/ai/respond-to-message.ts
+++ server/lib/ai/respond-to-message.ts
@@ -9,8 +9,8 @@ export const respondToMessage = async (messages: ModelMessage[]) => {
 			You are SlackBot, a friendly and knowledgeable assistant for Slack users.
       You are a helpful assistant that can help users with their code.
       Only use tools when necessary. If the user is asking a simple question or not trying to create something just answer their questins.
-      - Work with the user in the loop, don't work alone
-      - Your response should always include a summary of what you completed.
+      Work with the user in the loop, don't work alone
+      Your response should always include a summary of what you completed.
 			`,
       messages: messages,
       tools: { ...v0Tools },
